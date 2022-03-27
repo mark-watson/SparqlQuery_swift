58638d93de2bea717ff76fa9c5563ac71a3ed73c
Mark Watson <markw@markwatson.com>
2022-03-27 09:56:49 -0700

    -

--- Sources/SparqlQuery_swift/SparqlQuery_swift.swift
+++ Sources/SparqlQuery_swift/SparqlQuery_swift.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-import SwiftyJSON
-
 public func sparqlDbPedia(query: String) -> Array<Dictionary<String,String>> {
     return SparqlEndpointHelpter(query: query, endPointUri: "https://dbpedia.org/sparql?query=") }
 
@@ -9,10 +7,16 @@ public func sparqlWikidata(query: String) -> Array<Dictionary<String,String>> {
     return SparqlEndpointHelpter(query: query, endPointUri: "https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=") }
 
 public func SparqlEndpointHelpter(query: String, endPointUri: String) -> Array<Dictionary<String,String>> {
-    var ret = Array<Dictionary<String,String>>();
-    let requestUrl = URL(string: String(endPointUri + query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!) + "&format=json")!
+    var ret = Set<Dictionary<String,String>>();
     var content = "{}"
-    do { content = try String(contentsOf: requestUrl) } catch let error { print(error) }
+
+    let maybeString = cacheLookupQuery7(key: query)
+    if maybeString?.count ?? 0 > 0 {
+        content = maybeString ?? ""
+    } else {
+        let requestUrl = URL(string: String(endPointUri + query.addingPercentEncoding(withAllowedCharacters:     .urlHostAllowed)!) + "&format=json")!
+        do { content = try String(contentsOf: requestUrl) } catch let error { print(error) }
+    }
     let json = try? JSONSerialization.jsonObject(with: Data(content.utf8), options: [])
     if let json2 = json as! Optional<Dictionary<String, Any?>> {
         if let head = json2["head"] as? Dictionary<String, Any> {
@@ -28,5 +32,8 @@ public func SparqlEndpointHelpter(query: String, endPointUri: String) -> Array<D
                                         if let vals = (first_binding[key2]) {
                                             let vv : String = vals["value"] ?? "err2"
                                             ret2[key2] = vv } }
-                                    ret.append(ret2) }}}}}}}}
-    return ret }
+                                    if ret2.count > 0 {
+                                        ret.insert(ret2)
+                                    }}}}}}}}}
+    return Array(ret) }
+
--- Tests/SparqlQuery_swiftTests/main.swift
+++ Tests/SparqlQuery_swiftTests/main.swift
@@ -6,6 +6,6 @@ final class TempLibTests: XCTestCase {
     func testQueryCheck() {
         let z = sparqlDbPedia(query: "select * where { ?s ?p \"Steve Jobs\"@en } limit 5")
         print(z)
-        dump(z)
+        //dump(z)
     }
 }
